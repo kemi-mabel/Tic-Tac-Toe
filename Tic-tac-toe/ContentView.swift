cd748a62c231fcc93c53a049c233a1034d700f0b
Oluwakemi Onajinrin <oluwakemi.mabell@gmail.com>
2022-05-18 13:24:10 +0100

    Initial Commit

---
+++ Tic-tac-toe/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Tic-tac-toe
+//
+//  Created by Oluwakemi Onajinrin on 5/18/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Tic-tac-toe/Tic_tac_toeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tic_tac_toeApp.swift
+//  Tic-tac-toe
+//
+//  Created by Oluwakemi Onajinrin on 5/18/22.
+//
+
+import SwiftUI
+
+@main
+struct Tic_tac_toeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
